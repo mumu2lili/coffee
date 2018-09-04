3c7c78e22b42c272c23d954c76f0f76ae7969cd1
mumu2lili <451817970@qq.com>
2018-09-04 13:44:03 +0800

    mxgraph

---
+++ study/mxgraph/src/main/java/com/piggy/coffee/mxgraph/web/controller/GraphController.java
@@ -0,0 +1,22 @@
+package com.piggy.coffee.mxgraph.web.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("graph")
+public class GraphController {
+
+	@RequestMapping(value = "editor", method = RequestMethod.GET)
+	public String editor(Map<String, Object> model, HttpServletRequest req, HttpServletResponse res) {
+
+		return "editor";
+	}
+
+}
---
+++ study/mxgraph/src/main/resources/templates/index.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<#assign title = "目录" />
+<#include "/common/head.ftl">
+</head>
+<body>
+  <ul>
+    <li><a href="${basePath}graph/editor">流程编辑</a></li>
+  </ul>
+
+
+</body>
+</html>

f3f260e5262d5228485478ebb0d62208be7c2ee9
mm <451817970@qq.com>
2019-05-22 15:08:31 +0800

    websocket

---
+++ study/websocket/src/main/java/samples/websocket/tomcat/SampleTomcatWebSocketApplication.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package samples.websocket.tomcat;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import samples.websocket.tomcat.client.GreetingService;
+import samples.websocket.tomcat.client.SimpleGreetingService;
+import samples.websocket.tomcat.echo.DefaultEchoService;
+import samples.websocket.tomcat.echo.EchoService;
+import samples.websocket.tomcat.echo.EchoWebSocketHandler;
+import samples.websocket.tomcat.reverse.ReverseWebSocketEndpoint;
+import samples.websocket.tomcat.snake.SnakeWebSocketHandler;
+
+@EnableAutoConfiguration
+@EnableWebSocket
+public class SampleTomcatWebSocketApplication // extends SpringBootServletInitializer
+		implements WebSocketConfigurer {
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+
+		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+		protocol.setKeepAliveTimeout(1000 * 60);
+		protocol.setMaxKeepAliveRequests(2);
+
+		return connector;
+	}
+
+	// @Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoWebSocketHandler(), "/echo").withSockJS();
+		registry.addHandler(snakeWebSocketHandler(), "/snake").withSockJS();
+	}
+	//
+	// @Override
+	// protected SpringApplicationBuilder configure(SpringApplicationBuilder
+	// application) {
+	// return application.sources(SampleTomcatWebSocketApplication.class);
+	// }
+
+	@Bean
+	public EchoService echoService() {
+		return new DefaultEchoService("Did you say \"%s\"?");
+	}
+
+	@Bean
+	public GreetingService greetingService() {
+		return new SimpleGreetingService();
+	}
+
+	@Bean
+	public WebSocketHandler echoWebSocketHandler() {
+		return new EchoWebSocketHandler(echoService());
+	}
+
+	@Bean
+	public WebSocketHandler snakeWebSocketHandler() {
+		return new PerConnectionWebSocketHandler(SnakeWebSocketHandler.class);
+	}
+
+	@Bean
+	public ReverseWebSocketEndpoint reverseWebSocketEndpoint() {
+		return new ReverseWebSocketEndpoint();
+	}
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTomcatWebSocketApplication.class, args);
+	}
+
+}

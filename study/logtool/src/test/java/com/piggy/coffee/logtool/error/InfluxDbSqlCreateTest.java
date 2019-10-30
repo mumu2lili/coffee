79fe030d7c87f071021e0cb85753f3be474e2bf5
mm <451817970@qq.com>
2019-10-30 10:11:00 +0800

    a

--- study/logtool/src/test/java/com/piggy/coffee/logtool/error/InfluxDbSqlCreateTest.java
+++ study/logtool/src/test/java/com/piggy/coffee/logtool/error/InfluxDbSqlCreateTest.java
@@ -13,7 +13,7 @@ public class InfluxDbSqlCreateTest {
 	@Test
 	public void testCreate() throws IOException {
 
-		String str = "{\"tpiID\":\"713151\",\"podType\":0,\"instanceChallenge\":\"1\",\"timeLimit\":120,\"evaluateStartTime\":\"2019-10-25 17:02:28\",\"evaluateEndTime\":\"2019-10-25 17:03:41\"}";
+		String str = "{\"tpiID\":\"722334\",\"podType\":2,\"instanceChallenge\":\"2\",\"timeLimit\":60,\"evaluateStartTime\":\"2019-10-28 11:10:15\",\"evaluateEndTime\":\"2019-10-28 11:10:18\"}";
 
 		EvaluatingAssayParam param = JsonUtils.toBean(str, EvaluatingAssayParam.class);
 		createCpuUsage(param);
@@ -23,36 +23,59 @@ public class InfluxDbSqlCreateTest {
 	}
 
 	private void createCpuUsage(EvaluatingAssayParam assayParam) {
-		String cmd = "select time, container_name, nodename, pod_name, type, value from \"cpu/usage\" where time >= '%s' and time <= '%s' and pod_name =~ /.*-%s/;";
-		String startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8).minusSeconds(10)); // 东8区
-		String endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(30));// 加上收集周期10s
+		String cmd = "select time, container_name, nodename, pod_name, type, value from \"cpu/usage\" where time >= '%s' and time <= '%s' and pod_name =~ /.--%s/;";
+		System.out.println("--------cpu/usage--------");
+
+		System.out.println("--1");
+		String startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8)); // 东8区
+		String endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(10));// 加上收集周期10s
 		String podName = assayParam.tpiID;
 		cmd = String.format(cmd, startTime, endTime, podName);
-		System.out.println("********cpu/usage********");
 		System.out.println(cmd);
+
+		System.out.println("--2");
+		startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8).minusSeconds(10)); // 东8区
+		endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(30));// 加上收集周期10s
+		podName = assayParam.tpiID;
+		cmd = String.format(cmd, startTime, endTime, podName);
+		System.out.println(cmd);
+
+		System.out.println();
 	}
 
 	private void createCpuUsageRate(EvaluatingAssayParam assayParam) {
 		// 语句
 		String cmd = "select time, container_name, nodename, pod_name, type, value from \"cpu/usage_rate\" where time >= '%s' and time <= '%s' and type = 'node' and nodename = '%s';";
+		System.out.println("--------cpu/usage_rate--------");
+
+		System.out.println("--1");
 		String startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8)); // 东8区
 		String endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(10));// 加上收集周期10s
 		cmd = String.format(cmd, startTime, endTime, "");
-
-		System.out.println("********cpu/usage_rate********");
 		System.out.println(cmd);
+
+		System.out.println();
+
 	}
 
 	private void createMemoryPageFaultsRate(EvaluatingAssayParam assayParam) {
 
 		// 语句
-		String cmd = "select time, container_name, nodename, pod_name, type, value from \"memory/page_faults_rate\" where time >= '%s' and time <= '%s' and pod_name =~ /.*-%s/;";
-		String startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8).minusSeconds(10)); // 东8区
+		String cmd = "select time, container_name, nodename, pod_name, type, value from \"memory/page_faults_rate\" where time >= '%s' and time <= '%s' and pod_name =~ /.--%s/;";
+		System.out.println("--------cpu/page_faults_rate--------");
+
+		System.out.println("--1");
+		String startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8)); // 东8区
 		String endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(20));// 加上收集时间
 		String podName = assayParam.tpiID;
 		cmd = String.format(cmd, startTime, endTime, podName);
+		System.out.println(cmd);
 
-		System.out.println("********cpu/page_faults_rate********");
+		System.out.println("--2");
+		startTime = cvtTime(assayParam.getEvaluateStartTime().minusHours(8).minusSeconds(10)); // 东8区
+		endTime = cvtTime(assayParam.getEvaluateEndTime().minusHours(8).plusSeconds(20));// 加上收集时间
+		podName = assayParam.tpiID;
+		cmd = String.format(cmd, startTime, endTime, podName);
 		System.out.println(cmd);
 
 	}
@@ -64,7 +87,7 @@ public class InfluxDbSqlCreateTest {
 		cmd = String.format(cmd, assayParam.tpiID, assayParam.podType, assayParam.instanceChallenge,
 				assayParam.timeLimit, assayParam.evaluateStartTime, assayParam.evaluateEndTime);
 
-		System.out.println("********curl********");
+		System.out.println("--------curl--------");
 		System.out.println(cmd);
 
 	}

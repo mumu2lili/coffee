867c6686ab46a71c26abb4f128c999ab0fa01873
mm <451817970@qq.com>
2020-01-11 14:08:18 +0800

    ok

--- study/compiler/src/main/java/com/piggy/coffee/compiler/controller/CompilerController.java
+++ study/compiler/src/main/java/com/piggy/coffee/compiler/controller/CompilerController.java
@@ -11,16 +11,23 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.piggy.coffee.common.model.ApiResult;
+import com.piggy.coffee.compiler.model.ClassInfo;
+
+@RequestMapping("/compilers")
 @RestController
-public class CompileController {
+public class CompilerController {
 	private Logger log = LoggerFactory.getLogger(getClass());
 
 	private AtomicInteger i = new AtomicInteger(1);
 
 	@GetMapping("/hello")
-	public String helloWorld3() throws IOException {
+	public String helloWorld() throws IOException {
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		String s = "public class HelloWorld {\r\n" + "	public static void main(String[] args) {\r\n"
 				+ "		System.out.println(";
@@ -32,6 +39,19 @@ public class CompileController {
 		int r = compiler.run(null, null, null, path);
 		log.info("第{}次编译，结果{}", k, r);
 		return "hello";
+	}
+
+	@PostMapping("/compile")
+	public ApiResult<?> compile(@RequestBody ClassInfo classInfo) throws IOException {
+		ApiResult<?> result = new ApiResult<>();
+		String srcFile = classInfo.getSrcFile();
+		log.info("[start]编译oj {}文件{}", classInfo.getTpiId(), srcFile);
+
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		int r = compiler.run(null, null, null, srcFile);
+
+		log.info("[end]编译oj {}文件{},result:{}", classInfo.getTpiId(), srcFile, r);
+		return result;
 	}
 
 }
---
+++ study/compiler/src/main/java/com/piggy/coffee/compiler/model/ClassInfo.java
@@ -0,0 +1,24 @@
+package com.piggy.coffee.compiler.model;
+
+public class ClassInfo {
+
+	private String tpiId;
+	private String srcFile;
+
+	public String getTpiId() {
+		return tpiId;
+	}
+
+	public void setTpiId(String tpiId) {
+		this.tpiId = tpiId;
+	}
+
+	public String getSrcFile() {
+		return srcFile;
+	}
+
+	public void setSrcFile(String srcFile) {
+		this.srcFile = srcFile;
+	}
+
+}

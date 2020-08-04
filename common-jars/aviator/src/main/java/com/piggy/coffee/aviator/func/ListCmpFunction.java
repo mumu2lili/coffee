492a49a761f408fa252ac405bca85603e431d491
mumu <atomxx@qq.com>
2020-08-04 15:29:24 +0800

    note and aviator

---
+++ common-jars/aviator/src/main/java/com/piggy/coffee/aviator/func/ListCmpFunction.java
@@ -0,0 +1,99 @@
+package com.piggy.coffee.aviator.func;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Expression;
+import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.type.AviatorBoolean;
+import com.googlecode.aviator.runtime.type.AviatorJavaType;
+import com.googlecode.aviator.runtime.type.AviatorObject;
+
+/**
+ * filter(seq,predicate) to filter seq by predicate
+ *
+ * @author dennis
+ *
+ */
+public class ListCmpFunction extends AbstractFunction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public AviatorObject call(final Map<String, Object> env, final AviatorObject arg1) {
+
+		final AviatorObject arg2 = new AviatorJavaType("separator");
+		env.put("separator", " ");
+		
+		final AviatorObject arg3 = new AviatorJavaType("expect");
+		final AviatorObject arg4 = new AviatorJavaType("actual");
+
+		return call(env, arg1, arg2, arg3, arg4);
+	}
+	
+	@Override
+	public AviatorObject call(final Map<String, Object> env, final AviatorObject arg1, final AviatorObject arg2) {
+
+		final AviatorObject arg3 = new AviatorJavaType("expect");
+		final AviatorObject arg4 = new AviatorJavaType("actual");
+
+		return call(env, arg1, arg2, arg3, arg4);
+	}
+
+	@Override
+	public AviatorObject call(final Map<String, Object> env, final AviatorObject arg1, final AviatorObject arg2,
+			final AviatorObject arg3, final AviatorObject arg4) {
+
+		String exp = (String) arg1.getValue(env);
+
+		String separator = (String) arg2.getValue(env);
+		String expect = (String) arg3.getValue(env);
+		String actual = (String) arg4.getValue(env);
+
+		List<Double> eList = cvtToList(expect, separator);
+		List<Double> aList = cvtToList(actual, separator);
+		if (eList.size() != aList.size()) {
+
+		}
+
+		Expression expression = AviatorEvaluator.compile(exp, true);
+		Map<String, Object> p = new HashMap<>();
+		for (int i = 0; i < eList.size(); i++) {
+			Double e = eList.get(i);
+			Double a = aList.get(i);
+			p.put("expect", e);
+			p.put("actual", a);
+
+			// TODO 对结果类型进行判断
+			Boolean r = (Boolean) expression.execute(p);
+			if(!r) {
+				return AviatorBoolean.FALSE;
+			}
+		}
+
+		return AviatorBoolean.TRUE;
+	}
+
+	private List<Double> cvtToList(String str, String separator) {
+		List<Double> list = new ArrayList<>();
+		String[] arr = str.split(separator);
+		for (String s : arr) {
+			list.add(Double.parseDouble(s));
+		}
+
+		return list;
+
+	}
+
+	@Override
+	public String getName() {
+		return "list.cmp";
+	}
+
+}
--- common-jars/aviator/src/test/java/com/piggy/coffee/aviator/AviatorTest.java
+++ common-jars/aviator/src/test/java/com/piggy/coffee/aviator/AviatorTest.java
@@ -1,32 +1,82 @@
 package com.piggy.coffee.aviator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
 import com.googlecode.aviator.AviatorEvaluator;
+import com.piggy.coffee.aviator.func.ListCmpFunction;
 
 public class AviatorTest {
 
-
 	@Test
 	public void test() {
-		int[] a = new int[]{6, 7, 8, 9};
-        Map<String, Object> env = new HashMap<String, Object>();
-        env.put("a", a);
- 
-        System.out.println(AviatorEvaluator.execute("1 + 2 + 3"));
-        System.out.println(AviatorEvaluator.execute("a[1] + 100", env));
-        System.out.println(AviatorEvaluator.execute("'a[1]=' + a[1]", env));
-        //求数组长度
-        System.out.println(AviatorEvaluator.execute("count(a)", env));
-        //求数组总和
-        System.out.println(AviatorEvaluator.execute("reduce(a, +, 0)", env));
-        //检测数组每个元素都在 0 <= e < 10 之间。
-        System.out.println(AviatorEvaluator.execute("seq.every(a, seq.and(seq.ge(0), seq.lt(10)))", env));
+
+		System.out.println(AviatorEvaluator.execute("1 + 2 + 3"));
+
 	}
 
+	@Test
+	public void testArr() {
+		int[] a = new int[] { 6, 7, 8, 9 };
+		Map<String, Object> env = new HashMap<String, Object>();
+		env.put("a", a);
+
+		System.out.println(AviatorEvaluator.execute("a[1] + 100", env));
+	}
 	
+	@Test
+	public void testSeqFilter() {
+		Map<String, Object> env = new HashMap<String, Object>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(20);
+        list.add(10);
+        env.put("list", list);
+     
+    
+        List<?> result = (List<?>)AviatorEvaluator.execute("filter(list,seq.gt(9))", env);
+        System.out.println(result); 
+    
+	}
+	
+	@Test
+	public void testListCmp() throws IllegalAccessException, NoSuchMethodException {
+		Map<String, Object> env = new HashMap<String, Object>();
+        env.put("expect", "1 2 3");
+        env.put("actual", "1.01 2.01 3.01");
+    
+        AviatorEvaluator.addFunction(new ListCmpFunction());
+        boolean result = (boolean)AviatorEvaluator.execute("list.cmp('math.abs(expect - actual) < 0.1', ' ')", env);
+        System.out.println(result); 
+    
+	}
+	
+	@Test
+	public void testListCmp2() throws IllegalAccessException, NoSuchMethodException {
+		Map<String, Object> env = new HashMap<String, Object>();
+        env.put("expect", "1 2 3");
+        env.put("actual", "1.01 2.01 3.01");
+    
+        AviatorEvaluator.addFunction(new ListCmpFunction());
+        boolean result = (boolean)AviatorEvaluator.execute("list.cmp('math.abs(expect - actual) < 0.1')", env);
+        System.out.println(result); 
+    
+	}
+	
+	@Test
+	public void testListCmp_Tab() throws IllegalAccessException, NoSuchMethodException {
+		Map<String, Object> env = new HashMap<String, Object>();
+        env.put("expect", "1	2	3");
+        env.put("actual", "1.01	2.01	3.01");
+    
+        AviatorEvaluator.addFunction(new ListCmpFunction());
+        boolean result = (boolean)AviatorEvaluator.execute("list.cmp('math.abs(expect - actual) < 0.1', '	')", env);
+        System.out.println(result); 
+    
+	}
 
 }

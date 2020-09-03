0c0820f33bf06eef4b1dd87b3c260aaee9490245
mumu <atomxx@qq.com>
2020-09-03 18:20:12 +0800

    key

--- study/license/src/main/java/com/piggy/coffee/license/LicenseCreator2.java
+++ study/license/src/main/java/com/piggy/coffee/license/LicenseCreator2.java
@@ -1,19 +1,16 @@
 package com.piggy.coffee.license;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
-import java.security.KeyPair;
-import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMDecryptorProvider;
-import org.bouncycastle.openssl.PEMEncryptedKeyPair;
-import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.asn1.x509.Certificate;
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
 
 /**
  * CreateLicense
@@ -22,33 +19,41 @@ import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
  */
 public class LicenseCreator2 {
 
-	public static KeyPair readKeyPair(File pemFile, char[] password) throws Exception {
+	public static void readKeyPair(File pemFile, char[] password) throws Exception {
 		PEMParser pemParser = new PEMParser(new FileReader(pemFile));
 		Object object = pemParser.readObject();
 		pemParser.close();
-		PEMDecryptorProvider decProv = new JcePEMDecryptorProviderBuilder().build(password);
-		JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-		// 获得密钥对
-		KeyPair kp = null;
-		if (object instanceof PEMEncryptedKeyPair) {
-			kp = converter.getKeyPair(((PEMEncryptedKeyPair) object).decryptKeyPair(decProv));
-		} else {
-			kp = converter.getKeyPair((PEMKeyPair) object);
-		}
 
-		return kp;
+
+		X509CertificateHolder x = (X509CertificateHolder) object;
+
+		
+		
+		 CertificateFactory cf = null;
+	        PublicKey publicKey = null;
+	        try {
+	            cf = CertificateFactory.getInstance("X.509");
+	            //DataInputStream  di = new DataInputStream("");
+	            X509Certificate cert = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(x.getEncoded()));
+	            publicKey = cert.getPublicKey();
+	            System.out.println("ok");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	  
+
+
+	
 	}
 
-	public void initKeyPair(String pemFilePath, String password) {
-		byte[] data = "PEM".getBytes();
+	public PublicKey initKeyPair(String pemFilePath, String password) {
+	/*	byte[] data = "PEM".getBytes();
 		Security.addProvider(new BouncyCastleProvider());
-		KeyPair kp;
+		KeyPair kp;*/
 		try {
-			kp = (KeyPair) readKeyPair(new File(pemFilePath), password.toCharArray());
-			PublicKey publicKey = kp.getPublic();
-			PrivateKey privateKey = kp.getPrivate();
+			 readKeyPair(new File(pemFilePath), password.toCharArray());
 
-			return publicKey;
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -57,4 +62,9 @@ public class LicenseCreator2 {
 		return null;
 	}
 
+	
+	public static void main(String[] args) {
+		LicenseCreator2 lc = new LicenseCreator2();
+		lc.initKeyPair("D:\\work\\eclipse-workspace\\zs\\hnu.pem", "123456");
+	}
 }
--- study/license/src/test/resources/readme.txt
+++ study/license/src/test/resources/readme.txt
@@ -1,6 +1,6 @@
 ##################### bridge #############
 #bridge自签名证书
-keytool -genkeypair -alias bridgeca -dname "CN=mumu, OU=湖南智擎科技有限公司, O=实训云, L=长沙, ST=湖南, C=中国" -keypass 123456 -keyalg RSA -keysize 2048 -validity 3650 -keystore bridge.jks -storepass 123456 -storetype PKCS12
+keytool -genkeypair -alias bridgeca -dname "CN=mumu, OU=湖南智擎科技有限公司, O=实训云, L=长沙, ST=湖南, C=中国" -keypass 123456 -keyalg RSA -keysize 2048 -startdate 2020/09/03 -validity 3650 -keystore bridge.jks -storepass 123456 -storetype PKCS12
 
 #查看证书
 keytool -list -keystore bridge.jks -v
@@ -11,18 +11,19 @@ keytool -exportcert -alias bridgeca -file bridge.pem -rfc -keystore bridge.jks
 
 #####################湖南大学
 #湖南大学自签名证书
-keytool -genkeypair -alias hnu -dname "CN=管主任, OU=湖南大学, O=计算机实验室, L=长沙, ST=湖南, C=中国" -keypass 123456 -keyalg RSA -keysize 2048 -validity 3650 -keystore bridge.jks -storepass 123456 -storetype PKCS12
+keytool -genkeypair -alias hnu -dname "CN=管主任, OU=湖南大学, O=计算机实验室, L=长沙, ST=湖南, C=中国" -keypass 123456 -keyalg RSA -keysize 2048 -startdate 2020/09/03 -validity 730 -keystore bridge.jks -storepass 123456 -storetype PKCS12
 
 #签名请求
 keytool -certreq -alias hnu -file hnu.csr -keystore bridge.jks -storepass 123456
 keytool -printcertreq -file hnu.csr -v
 
 #证书签名
-keytool -gencert -infile hnu.csr -alias bridgeca -outfile hnu.pem -rfc -keystore bridge.jks -storepass 123456
+keytool -gencert -infile hnu.csr -alias bridgeca -startdate 2020/09/03 -validity 730 -outfile hnu.pem -rfc -keystore bridge.jks -storepass 123456
 keytool -printcert -file hnu.pem -v
 
 #签名后的证书导回密钥库中
 keytool -importcert -alias hnu -file hnu.pem -keystore bridge.jks -storepass 123456
 
-
+#导出私钥
+keytool -importcert -alias hnu -file hnu.pem -keystore bridge.jks -storepass 123456
 
